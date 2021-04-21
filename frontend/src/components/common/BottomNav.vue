6832b79f4dd9fa1288d533b5d718475a0190b9fd
김순석 <chaneli1194@naver.com>
2021-04-21 18:31:58 +0900

    S04P31B105-15 / feat: common componet 추가

---
+++ frontend/src/components/common/BottomNav.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="q-pa-md">
+    <q-layout view="lHh lpr lFf" container style="height: 400px" class="shadow-2 rounded-borders">
+      <q-footer bordered class="bg-white text-primary">
+        <q-tabs no-caps active-color="primary" indicator-color="transparent" class="text-grey" v-model="tab">
+          <q-tab name="feeds" label="Feeds" icon="comment-text-outline" />
+          <q-tab name="matching" label="Matching" />
+          <q-tab name="contents" label="Contents" />
+        </q-tabs>
+      </q-footer>
+    </q-layout>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      tab: 'images',
+    };
+  },
+};
+</script>
+
+<style></style>
---
+++ frontend/src/components/common/GoBack.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div>
+      <ion-icon name="chevron-back-outline">아이콘</ion-icon>
+    </div>
+    <div>
+      <q-btn color="" icon="chevron-left" label="" @click="goBack" />
+    </div>
+    <!-- <q-btn v-go-back="'/'" color="primary" /> -->
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    goBack() {
+      this.$router.go(-1);
+    },
+  },
+};
+</script>
+
+<style></style>
