10a9311100d299ea5282425c50c3e9ea38d16cdd
김순석 <chaneli1194@naver.com>
2021-04-28 10:33:49 +0900

    S04P31B105-19 / feat: three js 학습

--- threejs-practice/prac04_particle_systems/src/script.js
+++ threejs-practice/prac04_particle_systems/src/script.js
@@ -2,129 +2,36 @@ import './style.css';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import * as dat from 'dat.gui';
+// Instantiate a loader
+var loader = new THREE.GLTFLoader();
 
-// Texture Loader
-const loader = new THREE.TextureLoader();
-const cross = loader.load('./cross.jpg');
+// Optional: Provide a DRACOLoader instance to decode compressed mesh data
+// THREE.DRACOLoader.setDecoderPath('/examples/js/libs/draco');
+// loader.setDRACOLoader(new THREE.DRACOLoader());
 
-// Debug
-const gui = new dat.GUI();
+// Optional: Pre-fetch Draco WASM/JS module, to save time while parsing.
+// THREE.DRACOLoader.getDecoderModule();
 
-// Canvas
-const canvas = document.querySelector('canvas.webgl');
+// Load a glTF resource
+loader.load(
+  // resource URL
+  '/Totoro.glb',
+  // called when the resource is loaded
+  function (gltf) {
+    scene.add(gltf.scene);
 
-// Scene
-const scene = new THREE.Scene();
-
-// Objects
-const geometry = new THREE.TorusGeometry(0.7, 0.2, 16, 100);
-
-const particlesGeometry = new THREE.BufferGeometry();
-const particlesCnt = 5000;
-
-const posArray = new Float32Array(particlesCnt * 3);
-// xyz, xyz, xyz, xyz
-
-for (let i = 0; i < particlesCnt * 3; i++) {
-  //   posArray[i] = Math.random();
-  //   posArray[i] = Math.random() - 0.5;
-  //   posArray[i] = (Math.random() - 0.5) * 5;
-  posArray[i] = (Math.random() - 0.5) * (Math.random() * 5);
-}
-
-particlesGeometry.setAttribute('position', new THREE.BufferAttribute(posArray, 3));
-// Materials
-
-const material = new THREE.PointsMaterial({
-  //   transparent: true,
-  size: 0.005,
-});
-const particlesMaterial = new THREE.PointsMaterial({
-  size: 0.005,
-  map: cross,
-  transparent: true,
-  color: 'blue',
-});
-
-// Mesh
-const sphere = new THREE.Points(geometry, material);
-const particlesMesh = new THREE.Points(particlesGeometry, particlesMaterial);
-
-scene.add(sphere, particlesMesh);
-
-// Lights
-
-const pointLight = new THREE.PointLight(0xffffff, 0.1);
-pointLight.position.x = 2;
-pointLight.position.y = 3;
-pointLight.position.z = 4;
-scene.add(pointLight);
-
-/**
- * Sizes
- */
-const sizes = {
-  width: window.innerWidth,
-  height: window.innerHeight,
-};
-
-window.addEventListener('resize', () => {
-  // Update sizes
-  sizes.width = window.innerWidth;
-  sizes.height = window.innerHeight;
-
-  // Update camera
-  camera.aspect = sizes.width / sizes.height;
-  camera.updateProjectionMatrix();
-
-  // Update renderer
-  renderer.setSize(sizes.width, sizes.height);
-  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-});
-
-/**
- * Camera
- */
-// Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
-camera.position.x = 0;
-camera.position.y = 0;
-camera.position.z = 2;
-scene.add(camera);
-
-// Controls
-// const controls = new OrbitControls(camera, canvas)
-// controls.enableDamping = true
-
-/**
- * Renderer
- */
-const renderer = new THREE.WebGLRenderer({
-  canvas: canvas,
-});
-renderer.setSize(sizes.width, sizes.height);
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-
-/**
- * Animate
- */
-
-const clock = new THREE.Clock();
-
-const tick = () => {
-  const elapsedTime = clock.getElapsedTime();
-
-  // Update objects
-  sphere.rotation.y = 0.5 * elapsedTime;
-
-  // Update Orbital Controls
-  // controls.update()
-
-  // Render
-  renderer.render(scene, camera);
-
-  // Call tick again on the next frame
-  window.requestAnimationFrame(tick);
-};
-
-tick();
+    gltf.animations; // Array<THREE.AnimationClip>
+    gltf.scene; // THREE.Scene
+    gltf.scenes; // Array<THREE.Scene>
+    gltf.cameras; // Array<THREE.Camera>
+    gltf.asset; // Object
+  },
+  // called while loading is progressing
+  function (xhr) {
+    console.log((xhr.loaded / xhr.total) * 100 + '% loaded');
+  },
+  // called when loading has errors
+  function (error) {
+    console.log('An error happened');
+  }
+);
