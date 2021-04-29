1bc0392e8237d347a29f47f532fb67e847f60254
김윤빈 <kss759153@naver.com>
2021-04-29 10:18:01 +0900

    S04P31B105-110 / feat: create recommend meditation part

--- frontend/src/views/healing-content/ContentsHealingPage.vue
+++ frontend/src/views/healing-content/ContentsHealingPage.vue
@@ -2,7 +2,12 @@
   <div>
     <the-image-header class="relative" background-image="test.png" label="Contents"></the-image-header>
     <!-- TODO : 이전 페이지 이동 버튼 넣기-->
-    <div class="q-pa-lg flex row wrap justify-between">
+    <!-- TODO : 전체 버튼 배치 -->
+    <div class="inline-block absolute-right q-mr-lg q-pa-none cursor-pointer" style="margin-top: 6rem">
+      <q-icon name="tune"></q-icon>
+      <p class="inline-block q-ml-sm text-caption text-bold">전체</p>
+    </div>
+    <div class="q-pa-lg flex justify-between q-ma-none">
       <contents-card
         v-for="(contents, index) in contentsList" 
         :key="index" 
@@ -23,17 +28,42 @@ export default {
   },
   data() {
     return {
+      // json 파일 import 하기
       contentsList: [
         {
           category: 'GIF',
-          title: '힐링 콘텐츠',
-          contents: 'https://media.giphy.com/media/iDJXOU0SWNCPEsa5EL/giphy.gif',
+          title: 'Laughing',
+          contents: 'https://media3.giphy.com/media/3oEjHI8WJv4x6UPDB6/200.webp',
           bookmarked: false,
         },
         {
           category: '유튜브',
           title: '힐링 콘텐츠',
-          contents: "https://youtu.be/k3_tw44QsZQ",
+          contents: "https://youtu.be/77jy6yVLSH8",
+          bookmarked: false,
+        },
+        {
+          category: 'GIF',
+          title: 'Laughing',
+          contents: 'https://media3.giphy.com/media/3EjqRNFJmn0C4/200w.webp',
+          bookmarked: false,
+        },
+        {
+          category: '유튜브',
+          title: '힐링 콘텐츠',
+          contents: "https://youtu.be/8YX57oN1814",
+          bookmarked: false,
+        },
+        {
+          category: '유튜브',
+          title: '힐링 콘텐츠',
+          contents: "https://youtu.be/b-amjUifQqw",
+          bookmarked: false,
+        },
+        {
+          category: 'GIF',
+          title: 'Smile',
+          contents: 'https://media.giphy.com/media/QLvRBqfLXCphu/giphy.gif',
           bookmarked: false,
         },
         {
@@ -45,43 +75,31 @@ export default {
         {
           category: '유튜브',
           title: '힐링 콘텐츠',
-          contents: "https://youtu.be/k3_tw44QsZQ",
+          contents: "https://youtu.be/WXkJJaCNshY",
           bookmarked: false,
         },
         {
           category: '유튜브',
           title: '힐링 콘텐츠',
-          contents: "https://youtu.be/k3_tw44QsZQ",
+          contents: "https://youtu.be/3AQ2f2A6R4s",
           bookmarked: false,
         },
         {
           category: 'GIF',
           title: '힐링 콘텐츠',
-          contents: 'https://media.giphy.com/media/iDJXOU0SWNCPEsa5EL/giphy.gif',
+          contents: 'https://media.giphy.com/media/10t57cXgo7x5kI/giphy.gif',
           bookmarked: false,
         },
         {
           category: 'GIF',
-          title: '힐링 콘텐츠',
-          contents: 'https://media.giphy.com/media/iDJXOU0SWNCPEsa5EL/giphy.gif',
-          bookmarked: false,
-        },
-        {
-          category: '유튜브',
-          title: '힐링 콘텐츠',
-          contents: "https://youtu.be/k3_tw44QsZQ",
-          bookmarked: false,
-        },
-        {
-          category: '유튜브',
-          title: '힐링 콘텐츠',
-          contents: "https://youtu.be/k3_tw44QsZQ",
+          title: 'Love U',
+          contents: 'https://media.giphy.com/media/jRlP4zbERYW5HoCLvX/giphy.gif',
           bookmarked: false,
         },
         {
           category: 'GIF',
-          title: '힐링 콘텐츠',
-          contents: 'https://media.giphy.com/media/iDJXOU0SWNCPEsa5EL/giphy.gif',
+          title: 'CAT',
+          contents: 'https://media.giphy.com/media/GeimqsH0TLDt4tScGw/giphy.gif',
           bookmarked: false,
         },
       ]  
--- frontend/src/views/healing-content/ContentsMeditationPage.vue
+++ frontend/src/views/healing-content/ContentsMeditationPage.vue
@@ -1,12 +1,71 @@
 <template>
   <div>
-    meditation
+    <the-image-header class="relative" background-image="test.png" label="Meditation"></the-image-header>
+    <div class="q-pa-lg">
+      <div class="text-subtitle2 text-weight-bold q-mb-sm q-ml-xs">
+        추천명상
+      </div>
+      <!-- TODO : dots true 지정 가능하도록 -->
+      <article-carousel :number="1">
+        <contents-meditation 
+          v-for="(mainMeditation, index) in mainMeditationList" 
+          :key="index"
+          :entity="mainMeditation"
+        >
+        </contents-meditation>
+      </article-carousel>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
+import TheImageHeader from '@/components/common/TheImageHeader';
+import ContentsMeditation from '@/components/healing-content/ContentsMeditation';
+import ArticleCarousel from '@/components/article/ArticleCarousel.vue';
 
+export default {
+  components: {
+    TheImageHeader,
+    ContentsMeditation,
+    ArticleCarousel,
+  },
+  props: {
+    // 캐러셀 한 슬라이드에 보여줄 아이템의 수
+    number: {
+			type: Number,
+			default: 1
+		},
+  },
+  data() {
+    return {
+      mainMeditationList : [
+        {
+          id: 1,
+          time: 11,
+          title: '아침 명상',
+          background_img: 'https://cdn.quasar.dev/img/parallax2.jpg',
+          width: '100%',
+          height: '27vh',
+        },
+        {
+          id: 2,
+          time: 11,
+          title: '오후 명상',
+          background_img: 'https://cdn.quasar.dev/img/parallax2.jpg',
+          width: '100%',
+          height: '27vh',
+        },
+        {
+          id: 3,
+          time: 11,
+          title: '저녁 명상',
+          background_img: 'https://cdn.quasar.dev/img/parallax2.jpg',
+          width: '100%',
+          height: '27vh',
+        },
+      ]
+    }
+  }
 }
 </script>
 
