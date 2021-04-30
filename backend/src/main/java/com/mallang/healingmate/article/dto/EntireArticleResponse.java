f9c49febefe2f7aa9540e902fdb2f4cefd24ccdb
서범석 <sbseok12@gmail.com>
2021-04-30 21:24:45 +0900

    S04P31B105-116 / feat: 게시글 조회 기능을 위한 Response Dto 작성

---
+++ backend/src/main/java/com/mallang/healingmate/article/dto/EntireArticleResponse.java
@@ -0,0 +1,17 @@
+package com.mallang.healingmate.article.dto;
+
+import java.util.List;
+
+/**
+ * com.mallang.healingmate.article.dto
+ * EntireArticleResponse.java
+ * @date    2021-04-29 오후 2:52
+ * @author  서범석
+ *
+ * @변경이력
+ **/
+
+public class EntireArticleResponse {
+    private List<ArticleResponse> ArticleResponses;
+    private boolean isLast;
+}
