29a3eabc01819cebfbfe55e342f40813e289ba53
김윤빈 <kss759153@naver.com>
2021-04-30 17:16:05 +0900

    S04P31B105-135 / feat: change data format

--- frontend/src/views/healing-content/ContentsHealingPage.vue
+++ frontend/src/views/healing-content/ContentsHealingPage.vue
@@ -54,6 +54,8 @@
 <script>
 import TheImageHeader from '@/components/common/TheImageHeader';
 import ContentsCard from '@/components/healing-content/ContentsCard';
+import { data } from '@/assets/data/HealingContents.js';
+
 export default {
   components: {
     TheImageHeader,
@@ -62,81 +64,7 @@ export default {
   data() {
     return {
       contentsCategory: '전체',
-      // json 파일 import 하기
-      contents: [
-        {
-          category: 'GIF',
-          title: 'Laughing',
-          contents: 'https://media.giphy.com/media/3oEjHI8WJv4x6UPDB6/giphy.gif',
-          bookmarked: false,
-        },
-        {
-          category: '유튜브',
-          title: '힐링 콘텐츠1',
-          contents: "77jy6yVLSH8",
-          bookmarked: false,
-        },
-        {
-          category: 'GIF',
-          title: 'Laughing',
-          contents: 'https://media.giphy.com/media/l0ExayQDzrI2xOb8A/giphy.gif',
-          bookmarked: false,
-        },
-        {
-          category: '유튜브',
-          title: '힐링 콘텐츠2',
-          contents: "8YX57oN1814",
-          bookmarked: false,
-        },
-        {
-          category: '유튜브',
-          title: '힐링 콘텐츠3',
-          contents: "b-amjUifQqw",
-          bookmarked: false,
-        },
-        {
-          category: 'GIF',
-          title: 'Smile',
-          contents: 'https://media.giphy.com/media/QLvRBqfLXCphu/giphy.gif',
-          bookmarked: false,
-        },
-        {
-          category: 'GIF',
-          title: '힐링 콘텐츠',
-          contents: 'https://media.giphy.com/media/iDJXOU0SWNCPEsa5EL/giphy.gif',
-          bookmarked: false,
-        },
-        {
-          category: '유튜브',
-          title: '힐링 콘텐츠4',
-          contents: "WXkJJaCNshY",
-          bookmarked: false,
-        },
-        {
-          category: '유튜브',
-          title: '힐링 콘텐츠5',
-          contents: "3AQ2f2A6R4s",
-          bookmarked: false,
-        },
-        {
-          category: 'GIF',
-          title: '힐링 콘텐츠',
-          contents: 'https://media.giphy.com/media/10t57cXgo7x5kI/giphy.gif',
-          bookmarked: false,
-        },
-        {
-          category: 'GIF',
-          title: 'Love U',
-          contents: 'https://media.giphy.com/media/jRlP4zbERYW5HoCLvX/giphy.gif',
-          bookmarked: false,
-        },
-        {
-          category: 'GIF',
-          title: 'CAT',
-          contents: 'https://media.giphy.com/media/GeimqsH0TLDt4tScGw/giphy.gif',
-          bookmarked: false,
-        },
-      ]  
+      contents: data,
     }
   },
   computed: {
