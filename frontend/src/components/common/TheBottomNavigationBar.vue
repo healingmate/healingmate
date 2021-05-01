b753825d1b69a3f379747a1a1791cd6083e07ef2
김순석 <chaneli1194@naver.com>
2021-05-01 19:21:11 +0900

    S04P31B105-15 / fix: kebab, menu, notification, bottom nav 버튼 컴포넌트 수정

--- frontend/src/components/common/TheBottomNavigationBar.vue
+++ frontend/src/components/common/TheBottomNavigationBar.vue
@@ -1,6 +1,5 @@
 <template>
-  <div class="q-pa-md">
-    <!-- <q-layout view="lHh lpr lFf" container style="height: 400px" class="shadow-2 rounded-borders">
+  <!-- <q-layout view="lHh lpr lFf" container style="height: 400px" class="shadow-2 rounded-borders">
       <div class="nav">
         <q-footer bordered class="bg-white text-primary">
           <q-tabs no-caps active-color="primary" indicator-color="transparent" class="text-grey" v-model="tab">
@@ -12,12 +11,11 @@
       </div>
     </q-layout> -->
 
-    <q-tabs bordered no-caps active-color="primary" indicator-color="transparent" class="text-grey" v-model="tab">
-      <q-tab name="feeds" label="Feeds" icon="chat" />
-      <q-tab name="matching" label="Matching" icon="pets" />
-      <q-tab name="contents" label="Contents" icon="dynamic_feed" />
-    </q-tabs>
-  </div>
+  <q-tabs id="bottom-nav" bordered no-caps active-color="white" indicator-color="transparent" class="text-grey" v-model="tab">
+    <q-tab name="feeds" label="Feeds" icon="chat" />
+    <q-tab name="matching" label="Matching" icon="pets" />
+    <q-tab name="contents" label="Contents" icon="dynamic_feed" />
+  </q-tabs>
 </template>
 
 <script>
@@ -31,7 +29,12 @@ export default {
 </script>
 
 <style>
-.nav {
+#bottom-nav {
   position: absolute;
+  background-color: #244684;
+  width: 100%;
+}
+.q-tab {
+  color: #7b9ad3;
 }
 </style>
