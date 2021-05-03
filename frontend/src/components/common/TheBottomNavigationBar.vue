664727c752dfb28b9816a76659f780615ee06339
김순석 <chaneli1194@naver.com>
2021-05-03 17:16:33 +0900

    S04P31B105-15 / fix: bottom navbar component

--- frontend/src/components/common/TheBottomNavigationBar.vue
+++ frontend/src/components/common/TheBottomNavigationBar.vue
@@ -1,17 +1,5 @@
 <template>
-  <!-- <q-layout view="lHh lpr lFf" container style="height: 400px" class="shadow-2 rounded-borders">
-      <div class="nav">
-        <q-footer bordered class="bg-white text-primary">
-          <q-tabs no-caps active-color="primary" indicator-color="transparent" class="text-grey" v-model="tab">
-            <q-tab name="feeds" label="Feeds" icon="chat" />
-            <q-tab name="matching" label="Matching" icon="pets" />
-            <q-tab name="contents" label="Contents" icon="dynamic_feed" />
-          </q-tabs>
-        </q-footer>
-      </div>
-    </q-layout> -->
-
-  <q-tabs id="bottom-nav" bordered no-caps active-color="white" indicator-color="transparent" class="text-grey" v-model="tab">
+  <q-tabs id="nav" class="text-grey" bordered no-caps active-color="white" indicator-color="transparent" v-model="tab">
     <q-tab name="feeds" label="Feeds" icon="chat" />
     <q-tab name="matching" label="Matching" icon="pets" />
     <q-tab name="contents" label="Contents" icon="dynamic_feed" />
@@ -20,6 +8,13 @@
 
 <script>
 export default {
+  name: 'TheBottomNavigationBar',
+  props: {
+    width: {
+      type: String,
+      default: '100%',
+    },
+  },
   data() {
     return {
       tab: 'images',
@@ -29,11 +24,12 @@ export default {
 </script>
 
 <style>
-#bottom-nav {
+#nav {
   position: fixed;
-  background-color: #244684;
   width: 100%;
+  background-color: #244684;
   bottom: 0;
+  z-index: 100;
 }
 .q-tab {
   color: #7b9ad3;
--- frontend/src/views/bamboo/BambooEnterPage.vue
+++ frontend/src/views/bamboo/BambooEnterPage.vue
@@ -1,19 +1,15 @@
 <template>
-  <div :class="isNight ? 'home home--night-backgound-color' : 'home home--day-backgroud-color'">
-    <img
-      :src="require(isNight ? '@/assets/images/image-header/night_1x1.jpg' : '@/assets/images/image-header/day_1x1.jpg')"
-      alt="사진을 불러오지 못했어요."
-      style="width: 100%;"
-      class="layered-image"
-    />
+  <div :class="'home home--night-backgound-color'">
+    <img :src="require('@/assets/images/image-header/bamboo-main.jpg')" alt="사진을 불러오지 못했어요." style="width: 100%;" />
+
     <div class="home_saying home--night-font-color p-y-28">
       <p class="home_saying home--night-font-color" style="font-size: 40px">Bamboo Forest</p>
       <p class="home_saying home--night-font-color" style="text-align: right; font-size: 15px">고민을 나누는 익명의 공간</p>
     </div>
-    <div class="absolute p-y-28" style="bottom: 20px; width: 100%;">
-      <BaseButton style="padding: 0 28px;" back-ground-color="#ffffff" text-color="#244684" label="입장하기" @click.native="onEnterButton" />
+    <div class="p-y-28" style="margin-top: 30px;">
+      <BaseButton back-ground-color="#ffffff" text-color="#244684" label="입장하기" @click.native="onEnterButton" />
     </div>
-    <TheBottomNavigationBar style="width: 100%;" />
+    <TheBottomNavigationBar :width="width" />
   </div>
 </template>
 
@@ -32,9 +28,11 @@ export default {
   props: {
     isNight: Boolean,
   },
-  // data() {
-  //   return {}
-  // },
+  data() {
+    return {
+      width: '100%',
+    };
+  },
   // computed: {},
   // watch: {},
   // created() {},
@@ -60,7 +58,7 @@ export default {
 }
 
 .home--night-backgound-color {
-  background-color: #000934;
+  background-color: #171e3a;
 }
 
 .home--night-font-color {
