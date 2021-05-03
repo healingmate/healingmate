af8d91901c4a769f9c5b2d12d906c711da494dc6
서범석 <sbseok12@gmail.com>
2021-05-03 21:50:40 +0900

    S04P31B105-116 / feat: 차단 당하지 않은 유저의 게시물만 조회하는 기능 추가

---
+++ backend/src/main/java/com/mallang/healingmate/article/repository/ArticleRepository.java
@@ -0,0 +1,24 @@
+package com.mallang.healingmate.article.repository;
+
+import com.mallang.healingmate.account.domain.Account;
+import com.mallang.healingmate.article.domain.Article;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+/**
+ * com.mallang.healingmate.article.service
+ * ArticleRepository.java
+ * @date    2021-04-29 오후 2:44
+ * @author  서범석
+ *
+ * @변경이력
+ * 21-05-03 페이징 처리를 포함한 조회 기능 구현
+ **/
+
+public interface ArticleRepository extends JpaRepository<Article, Long> {
+
+    List<Article> findAllByAccountNotIn(List<Account> banToByBanFrom, Pageable pageable);
+}
