b04ab38710b612683593dd611f0a68a8661f5a7f
이동규 <uykgnod@naver.com>
2021-05-12 15:42:02 +0900

    S04P31B105-132 / fix: cookie 인코딩 문제로 인한 수정

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,21 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-// import { Cookies } from 'quasar'
-import { getCookie } from '@/utils/cookies'
+import state from '@/store/state'
 import mutations from '@/store/mutations'
 import actions from '@/store/actions'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-    accessToken: getCookie('access_token') || null,
-    refreshToken: getCookie('refresh_token') || null,
-    userId: getCookie('user_id') || null,
-    nickname: getCookie('nickname') || null,
-    profileImage: getCookie('profile_image') || null,
-    keyword: getCookie('keyword') || null,
-  },
+  state,
   mutations,
   actions,
   modules: {}
