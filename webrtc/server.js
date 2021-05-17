e173c2ce5438926918253c6de7a59687e795be32
LEEAYOUNG <souk0711@gmail.com>
2021-05-17 12:23:29 +0900

    S04P31B105-248 / feat : webrtc 서버 코드 수정

--- webrtc/server.js
+++ webrtc/server.js
@@ -284,7 +284,7 @@ var ioOptions = {
 
 ioServer(httpApp, ioOptions).on("connection", function (socket) {
   RTCMultiConnectionServer.addSocket(socket, config);
-
+  
   // ----------------------
   // below code is optional
 
@@ -297,58 +297,4 @@ ioServer(httpApp, ioOptions).on("connection", function (socket) {
   socket.on(params.socketCustomEvent, function (message) {
     socket.broadcast.emit(params.socketCustomEvent, message);
   });
-
-  // 여기부터는 내가 커스텀
-  socket.userData = { x: 0, y: 0, z: 0, heading: 0 }; //Default values;
-
-  console.log(`${socket.id} connected`);
-  socket.emit("setId", { id: socket.id });
-
-  socket.on("disconnectCharacter", function () {
-    console.log(`Player ${socket.id} disconnected`);
-    socket.broadcast.emit("deletePlayer", { id: socket.id });
-  });
-
-  socket.on("init", function (data) {
-    console.log(`socket.init ${data.model}`);
-    socket.userData.model = data.model;
-    socket.userData.colour = data.colour;
-    socket.userData.x = data.x;
-    socket.userData.y = data.y;
-    socket.userData.z = data.z;
-    socket.userData.heading = data.h;
-    (socket.userData.pb = data.pb), (socket.userData.action = "Idle");
-  });
-
-  socket.on("update", function (data) {
-    socket.userData.x = data.x;
-    socket.userData.y = data.y;
-    socket.userData.z = data.z;
-    socket.userData.heading = data.h;
-    (socket.userData.pb = data.pb), (socket.userData.action = data.action);
-  });
 });
-
-setInterval(function () {
-    const nsp = io.of("/");
-    let pack = [];
-  
-    for (let id in io.sockets.sockets) {
-      const socket = nsp.connected[id];
-      //Only push sockets that have been initialised
-      if (socket.userData.model !== undefined) {
-        pack.push({
-          id: socket.id,
-          model: socket.userData.model,
-          colour: socket.userData.colour,
-          x: socket.userData.x,
-          y: socket.userData.y,
-          z: socket.userData.z,
-          heading: socket.userData.heading,
-          pb: socket.userData.pb,
-          action: socket.userData.action,
-        });
-      }
-    }
-    if (pack.length > 0) io.emit("remoteData", pack);
-  }, 40);
