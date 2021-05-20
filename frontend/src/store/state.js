b65e3fe619491c349b6376b02aafb80ec92b4242
이동규 <uykgnod@naver.com>
2021-05-21 03:31:12 +0900

    hotfix

--- frontend/src/store/state.js
+++ frontend/src/store/state.js
@@ -6,5 +6,5 @@ export default {
   userId: getCookie('user_id') || null,
   nickname: decodeURIComponent(getCookie('nickname')) || null,
   profileImage: getCookie('profile_image') || null,
-  keyword: decodeURIComponent(getCookie('keyword')) || [],
+  keyword: decodeURIComponent(getCookie('keyword')) || null,
 };
